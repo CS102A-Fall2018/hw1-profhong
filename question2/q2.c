7242f7299e0a668a7d814107578fa1718ddcacd4
Chris Hong <hong3@cooper.edu>
2018-09-11 19:23:31 -0400

    added q2

---
+++ question2/q2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Failure is not fatal; failure to change is.\n");
+    printf("Failure is not fatal; failure to change is.\n");
+    printf("Failure is not fatal; failure to change is.\n");
+    //printf("Failure is not fatal; failure to change is.\n");
+    return 0;
+}
